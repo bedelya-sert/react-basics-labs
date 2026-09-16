7189be449f1c32876be5bd1b8f5054a80b082af7
Daz <butterflydazzle2@gmail.com>
2026-09-16 11:04:03 +0100

    Second commit.
    Save
    Save

---
+++ src/components/Task.jsx
@@ -0,0 +1,10 @@
+const Task = (props) => {
+    
+    return (
+        <div>
+            <p>{props.title}</p>
+            <p>Due: {props.deadline}</p>
+        </div>
+    )
+}
+export default Task;
